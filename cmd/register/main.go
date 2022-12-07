58ae04424f737e25813d929a1c016acc605cc5ac
Brody Taylor <59207664+brody-taylor@users.noreply.github.com>
2022-12-07 00:47:52 -0500

    Added flag to clear registerd discord commands

--- cmd/register/main.go
+++ cmd/register/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"flag"
+
 	"game-server/internal/config"
 	"game-server/internal/discordbot/discordcmd"
 )
 
+var doClear = flag.Bool("clear", false, "clear all currently registered commands")
+
 func main() {
+	flag.Parse()
+
 	cfg := config.New()
 	if err := cfg.Load(); err != nil {
 		panic(err)
@@ -15,7 +21,14 @@ func main() {
 	if err := c.Connect(); err != nil {
 		panic(err)
 	}
-	if err := c.Register(); err != nil {
+
+	var err error
+	if *doClear {
+		err = c.Clear()
+	} else {
+		err = c.Register()
+	}
+	if err != nil {
 		panic(err)
 	}
 }
